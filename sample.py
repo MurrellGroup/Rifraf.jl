85bf15870672dd472326cdc4729329f831c027aa
Kemal Eren <kemal@kemaleren.com>
2016-02-01 13:44:45 -0800

    sample from templates; including insertions and deletions

--- sample.py
+++ sample.py
@@ -3,25 +3,11 @@ import random
 from itertools import repeat, chain
 
 import numpy as np
-from numpy.random import beta
-from numpy.random import exponential
+from numpy.random import poisson
 
 
 def phred(p):
     return -10 * np.log10(p)
-
-
-def sample_error_ps(length, errors):
-    """Probability of error for each base."""
-    mean = errors / length
-    a = 1
-    b = (1 - mean) / mean
-    return beta(a, b, size=length)
-    
-
-def sample_expected_errors(mean, n):
-    """Expected number of errors for a read"""
-    return exponential(mean, size=n)
 
 
 def sample_template(length):
@@ -32,32 +18,25 @@ def mutate_base(base):
     return random.choice(list(set('ACGT') - set(base)))
 
 
-def mutate_template(template, identity):
-    max_errors = np.floor(len(template) * (1 - identity) / 2)
-    if max_errors < 1:
-        raise Exception('identity is too stringent: {}'.format(identity))
-    n_errs = random.randint(1, max_errors)
-    positions = list(sorted(random.sample(range(len(template)), n_errs)))
-    mutations = list(mutate_base(template[p]) for p in positions)
-    result = list(template)
-    for i, c in zip(positions, mutations):
-        result[i] = c
-    return ''.join(result)
-
-
 def coinflip(p):
     return random.random() < p
 
 
-def read_base(base, p):
-    if coinflip(p):
-        return mutate_base(base)
-    return base
-
-
-def generate_read(template, ps):
-    return ''.join(list(read_base(c, p) for c, p in zip(template, ps)))
-
+def sample_from_template(template, point_rate, insertion_rate, deletion_rate):
+    result = []
+    for base in template:
+        while coinflip(insertion_rate):
+            result.append(random.choice('ACGT'))
+        if coinflip(deletion_rate):
+            continue
+        if coinflip(point_rate):
+            result.append(mutate_base(base))
+        else:
+            result.append(base)
+    while coinflip(insertion_rate):
+        result.append(random.choice('ACGT'))
+    return ''.join(result)
+        
 
 def repeat_and_chain(items, ns):
     """
@@ -69,80 +48,29 @@ def repeat_and_chain(items, ns):
     return chain.from_iterable(repeat(item, n) for item, n in zip(items, ns))
 
 
-def sample(identity, ns, length=1000, error_mean=5, shuffle=True):
-    """Generate similar templates, and sample from them.
+def sample(n, length, mean_error_rate, insertion_rate, deletion_rate):
+    """Generate a template, and sample from it.
 
     Paramters
     ---------
-    identity : float
-        In range [0, 1]. Minimum pairwise identity for all templates.
-
-    ns : list of integers
+    s : list of integers
         Number of sequences to sample from each template.
 
     length : integer
         Length of sequences
 
-    error_mean : float
-        Mean number of expected errors per sequence.
-
-    Examples
-    --------
-    sample(0.99, [50, 50, 50], length=1000, error_mean=10)
+    mean_error_rate : float
+        Mean number of expected point errors per sequence.
 
     """
-    base_template = sample_template(length)
-    templates = list(mutate_template(base_template, identity) for _ in range(len(ns)))
-    errors = sample_expected_errors(error_mean, sum(ns))
-    ps = np.vstack(list(sample_error_ps(length, e) for e in errors))
-    reads = list(generate_read(template, qv) for template, qv in zip(repeat_and_chain(templates, ns), ps))
-    template_indices = list(repeat_and_chain(range(len(templates)), ns))
-    if shuffle:
-        indices = list(range(sum(ns)))
-        random.shuffle(indices)
-        ps = ps[indices]
-        reads = list(reads[i] for i in indices)
-        template_indices = list(template_indices[i] for i in indices)
-    return seqs_to_array(templates), ps, seqs_to_array(reads), np.array(template_indices)
-
-
-def seqs_to_array(reads):
-    convert = {'A': 0, 'C': 1, 'G': 2, 'T': 3}
-    return np.vstack(list(convert[base] for base in read) for read in reads)
-
-
-def array_to_seq(a):
-    convert = 'ACGT'
-    return ''.join(convert[i] for i in a)
-
-
-def lognormalize(x):
-    """normalize log values so they are log probabilities
-    
-    >>> np.all(lognormalize([np.log(.2), np.log(.2)]) == np.array([np.log(.5), np.log(.5)]))
-    True
-
-    """
-    a = np.logaddexp.reduce(x)
-    return x - a
-
-
-def column_distribution(bases, ps):
-    result = np.zeros(4)
-    for base, p in zip(bases, ps):
-        for cand in range(len(result)):
-            if base == cand:
-                result[cand] += np.log1p(-p)
-            else:
-                result[cand] += np.log(p)
-    return lognormalize(result)
-
-
-def estimate_template(read_array, ps):
-    # TODO: do in terms of log-sum-exp
-    dists = np.vstack(list(column_distribution(read_array[:, j], ps[:, j]) for j in range(read_array.shape[1])))
-    return dists
-
-
-def template_entropy(dists):
-    return -(dists * np.exp(dists)).sum(axis=1)
+    reads = []
+    ps = []
+    template = sample_template(length)
+    for i in range(n):
+        point_rate = poisson(mean_error_rate)
+        reads.append(sample_from_template(template,
+                                          point_rate / length,
+                                          insertion_rate / length,
+                                          deletion_rate / length))
+        ps.append(list(repeat(point_rate / length, length)))
+    return template, reads, ps
