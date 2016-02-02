db26e07f1d62c84cf8108fb75ac616bc18033456
Kemal Eren <kemal@kemaleren.com>
2016-02-02 13:41:00 -0800

    got basic version of quiver2 working

--- quiver2.py
+++ quiver2.py
@@ -1,14 +1,27 @@
 import numpy as np
 
 
+def seq_to_array(seq):
+    convert = {'A': 0, 'C': 1, 'G': 2, 'T': 3}
+    return np.array(list(convert[base] for base in seq))
+
+
+def array_to_seq(a):
+    convert = 'ACGT'
+    return ''.join(convert[i] for i in a)
+
+
 def forward(s, qv, t, log_ins, log_del):
-    result = np.zeros((len(s), len(t)))
-    for i in range(len(s)):
-        for j in range(len(t)):
+    result = np.zeros((len(s) + 1, len(t) + 1))
+    # invariant: result[i, j] is prob of aligning s[:i] to t[:j]
+    result[:, 0] = log_ins * np.arange(len(s) + 1)
+    result[0, :] = log_del * np.arange(len(t) + 1)
+    for i in range(1, len(s) + 1):
+        for j in range(1, len(t) + 1):
             result[i, j] = max([result[i - 1, j] + log_ins,  # insertion
                                 result[i, j - 1] + log_del,  # deletion
                                 # TODO: phred(1-p) may not be negligible
-                                result[i - 1, j - 1] + (0 if s[i] == t[j] else qv[i])])
+                                result[i - 1, j - 1] + (0 if s[i - 1] == t[j - 1] else qv[i - 1])])
     return result
 
 
@@ -68,17 +81,18 @@ def mutate(template):
     yield result
 
 
-def _quiver2(sequences, phreds, log_ins, log_del, maxiter=100):
+def quiver2(sequences, phreds, log_ins, log_del, maxiter=100):
     """
-    sequences: list of numpy arrays
+    sequences: list of dna sequences
 
     phreds: list of numpy array
 
     """
-    log_ps = -phreds
+    seq_arrays = list(seq_to_array(s) for s in sequences)
+    log_ps = list(-p for p in phreds)
     # choose first sequence as initial template
     # TODO: choose highest quality sequence as a template
-    template = np.copy(sequences[0])
+    template = np.copy(seq_arrays[0])
 
     # # compute forward matrices A and backward matrices B
     # As = list(forward(s, p, template, log_ins, log_del) for s, p in zip(sequences, phreds))
@@ -88,13 +102,14 @@ def _quiver2(sequences, phreds, log_ins, log_del, maxiter=100):
     for i in range(maxiter):
         old = template
         best = template
-        best_score = score(template, sequences, log_ps, log_ins, log_del)
+        best_score = score(template, seq_arrays, log_ps, log_ins, log_del)
+        # TODO: score candidates with column trick
         for cand in mutate(template):
-            cand_score = score(cand, sequences, log_ps, log_ins, log_del)
+            cand_score = score(cand, seq_arrays, log_ps, log_ins, log_del)
             if cand_score > best_score:
                 best = cand
                 best_score = cand_score
         template = best
         if np.all(old == template):
             break
-    return template
+    return array_to_seq(template)
