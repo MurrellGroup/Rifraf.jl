2d56110ec04b3fbca18aebf01e606a1038ba7ac0
Kemal Eren <kemal@kemaleren.com>
2016-02-05 12:13:50 -0800

    add some checks to BandedMatrix

--- quiver2.py
+++ quiver2.py
@@ -5,18 +5,19 @@ from poapy.poagraph import POAGraph
 from poapy.seqgraphalignment import SeqGraphAlignment
 
 # FIXME: asymmetrical bandwidth is wrong thing to do. offset so it is symmetric.
-# FIXME: BandedMatrix is wrong when bandwidth too small for rectangular shape
-
+# TODO: test BandedMatrix
+# TODO: improve slicing and arithmetic operations of BandedMatrix
 # TODO: port to Julia and compare speed
 
 # TODO: use faster version of partial order aligner
-# TODO: try column-major order
-# TODO: detect when overflow possible
-# TODO: unsigned ints where possible
 # TODO: benchmark against actual quiver implementation
 
 
-class OutsideBandException(Exception):
+class OutsideBandError(Exception):
+    pass
+
+
+class OutOfBoundsError(Exception):
     pass
 
 
@@ -31,7 +32,7 @@ class BandedMatrix(object):
     def __init__(self, shape, bandwidth, offset=0):
         nrows, ncols = shape
         if not nrows > 0 and ncols > 0:
-            raise Exception()
+            raise Exception('shape is not positive: {}'.format(shape))
         self.shape = shape
         self.bandwidth = bandwidth
         self.data = np.zeros((2 * bandwidth + 1, ncols))
@@ -45,23 +46,49 @@ class BandedMatrix(object):
     def ncols(self):
         return self.shape[1]
 
-    def _convert_row(self, i, j, o=0):
-        if i < max(j - self.bandwidth - self.offset, 0):
-            raise OutsideBandException()
-        if i > min(j + self.bandwidth - self.offset + o, self.nrows - (1 - o)):
-            raise OutsideBandException()
-        return i - (j - self.bandwidth - self.offset)
+    def check_row(self, i):
+        if i < 0:
+            i = self.nrows + i
+        if i >= self.nrows:
+            raise OutOfBoundsError()
+        return i
 
-    def range(self, j):
+    def check_col(self, j, newcol=False):
+        """if newcol is true, allow j=ncols, for inserting a new column"""
+        if j < 0:
+            j = self.ncols + j
+        if j >= self.ncols + (1 if newcol else 0):
+            raise OutOfBoundsError()
+        return j
+
+    def _convert_row(self, i, j, extend=False):
+        """convert array row to data row"""
+        o = 1 if extend else 0
+        if i < max(j - self.bandwidth - self.offset, 0):
+            raise OutsideBandError()
+        if i > min(j + self.bandwidth - self.offset + o, self.nrows - (1 - o)):
+            raise OutsideBandError()
+        result = i - (j - self.bandwidth - self.offset)
+        if result < 0 or result - o >= self.data.shape[0]:
+            raise OutOfBoundsError()
+        return result
+
+    def range(self, j, newcol=False):
+        """array-indexed row range of band in column j"""
+        j = self.check_col(j, newcol)
         start = max(j - self.bandwidth - self.offset, 0)
         stop = min(j - self.offset + self.bandwidth + 1, self.nrows)
         assert start < stop
+        if start == stop:
+            raise OutOfBoundsError()
         return start, stop
 
     def data_range(self, j):
+        """row range for column j in self.data"""
+        j = self.check_col(j)
         start, stop = self.range(j)
         dstart = self._convert_row(start, j)
-        dstop = self._convert_row(stop, j, o=1)
+        dstop = self._convert_row(stop, j, extend=True)
         assert dstart < dstop
         return dstart, dstop
 
@@ -70,16 +97,15 @@ class BandedMatrix(object):
         return i >= start and i < stop
 
     def get_elt(self, i, j):
-        if i < 0:
-            i = self.nrows + i
-        if j < 0:
-            j = self.ncols + j
+        i = self.check_row(i)
+        j = self.check_col(j)
         if self.inband(i, j):
             row = self._convert_row(i, j)
             return self.data[row, j]
         return 0
 
     def get_col(self, j):
+        j = self.check_col(j)
         start, stop = self.data_range(j)
         return self.data[start:stop, j]
 
@@ -99,6 +125,8 @@ class BandedMatrix(object):
     def __setitem__(self, key, val):
         # TODO: implement multidim slicing and negative indices
         i, j = key
+        i = self.check_row(i)
+        j = self.check_col(j)
         row = self._convert_row(i, j)
         self.data[row, j] = val
 
@@ -116,15 +144,15 @@ def update(arr, i, j, actual_j, s_base, t_base,
     # TODO: log(1-p) may not be negligible
     sub = (0 if s_base == t_base else log_p)
     if i == actual_j - bandwidth:
-        return max(arr.get_elt(i, j - 1) + log_del,  # deletion
-                   arr.get_elt(i - 1, j - 1) + sub)
+        return max(arr[i, j - 1] + log_del,  # deletion
+                   arr[i - 1, j - 1] + sub)
     elif i == actual_j + bandwidth:
-        return max(arr.get_elt(i - 1, j) + log_ins,  # insertion
-                   arr.get_elt(i - 1, j - 1) + sub)
+        return max(arr[i - 1, j] + log_ins,  # insertion
+                   arr[i - 1, j - 1] + sub)
     elif actual_j - bandwidth < i < actual_j + bandwidth:
-        return max(max(arr.get_elt(i - 1, j) + log_ins,  # insertion
-                       arr.get_elt(i, j - 1) + log_del),  # deletion
-                   arr.get_elt(i - 1, j - 1) + sub)
+        return max(max(arr[i - 1, j] + log_ins,  # insertion
+                       arr[i, j - 1] + log_del),  # deletion
+                   arr[i - 1, j - 1] + sub)
     else:
         raise Exception('update called outside bandwidth')
 
@@ -191,7 +219,7 @@ def score_mutation(mutation, template, seq, log_p, A, B, log_ins, log_del, bandw
     Bcol = B[:, bj]
 
     # need to chop off beginning of Acol and end of Bcol and align
-    a_start, a_stop = A.range(aj)
+    a_start, a_stop = A.range(aj, newcol=(mtype == "insertion" and aj == A.shape[1]))
     b_start, b_stop = B.range(bj)
 
     amin = max(b_start - a_start, 0)
