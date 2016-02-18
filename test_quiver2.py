c8ee4dceea0a87dd5542ca5b9563093e392e33c3
Kemal Eren <kemal@kemaleren.com>
2016-02-17 17:38:33 -0800

    update tests.

--- test_quiver2.py
+++ test_quiver2.py
@@ -65,23 +65,24 @@ class TestQuiver2(unittest.TestCase):
     def test_updated_col_substitution(self):
         log_del = -10
         log_ins = -5
-        template = 'AAAA'
-        seq = 'ATAA'
+        template = 'TAAG'
+        seq = 'ATAG'
+        pos = 1
         log_p = np.repeat(-3, len(seq))
         A = forward(seq, log_p, template, log_ins, log_del, bandwidth=1)
-        result = updated_col(1, 'T', template, seq, log_p, A, log_ins, log_del)
-        expected = forward(seq, log_p, seq, log_ins, log_del, bandwidth=1).get_col(2)
+        result = updated_col(pos, 'T', template, seq, log_p, A, log_ins, log_del)
+        expected = forward(seq, log_p, 'TTAG', log_ins, log_del, bandwidth=1).get_col(pos + 1)
         assert_array_equal(result, expected)
 
     def test_updated_col_insertion(self):
         log_del = -10
         log_ins = -5
-        template = 'AAA'
-        seq = 'AAAT'
+        template = 'AAAAA'
+        seq = 'AAAAAT'
         log_p = np.repeat(-3, len(seq))
         A = forward(seq, log_p, template, log_ins, log_del, bandwidth=1)
-        result = updated_col(3, 'T', template, seq, log_p, A, log_ins, log_del)
-        expected = forward(seq, log_p, seq, log_ins, log_del, bandwidth=1).get_col(4)
+        result = updated_col(len(template), 'T', template, seq, log_p, A, log_ins, log_del)
+        expected = forward(seq, log_p, seq, log_ins, log_del, bandwidth=1).get_col(len(template) + 1)
         assert_array_equal(result, expected)
 
     def test_random_mutations(self):
@@ -98,23 +99,31 @@ class TestQuiver2(unittest.TestCase):
             bandwidth = max(2 * np.abs(len(template) - len(seq)), 5)
             f = random.choice(['substitution', 'insertion', 'deletion'])
             maxpos = len(template) if f == 'insertion' else len(template) - 1
+            pos = random.randint(0, maxpos)
+            base = random.choice('ACGT')
             mutation = (f,
-                        random.randint(0, maxpos),
-                        random.choice('ACGT'))
+                        pos,
+                        base)
             new_template = update_template(template, mutation)
             phreds = phred(np.repeat(point_rate + insertion_rate + deletion_rate, len(seq)))
             log_p = -phreds / 10
             A = forward(seq, log_p, template, log_ins, log_del, bandwidth)
             B = backward(seq, log_p, template, log_ins, log_del, bandwidth)
-            score = score_mutation(mutation, template, seq, log_p, A, B, log_ins, log_del, bandwidth)
-            score2 = score_slow(new_template, seq, log_p, log_ins, log_del, bandwidth)
             M = forward(seq, log_p, new_template, log_ins, log_del, bandwidth)
             nr, nc = M.shape
+            # if f != 'deletion':
+            #     col = updated_col(pos, base, template, seq, log_p, A, log_ins, log_del)
+            #     exp_col = M.get_col(pos + (0 if f == 'deletion' else 1))
+            #     assert_array_equal(col, exp_col)
+            score = score_mutation(mutation, template, seq, log_p, A, B, log_ins, log_del, bandwidth)
+            score2 = score_slow(new_template, seq, log_p, log_ins, log_del, bandwidth)
             score3 = M.get_elt(nr - 1, nc - 1)
             score4 = backward(seq, log_p, new_template, log_ins, log_del, bandwidth).get_elt(0, 0)
             self.assertAlmostEqual(score, score2)
             self.assertAlmostEqual(score, score3)
             self.assertAlmostEqual(score, score4)
+
+
 
 
 if __name__ == '__main__':
