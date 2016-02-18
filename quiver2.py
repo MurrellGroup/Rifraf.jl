2ea4342b792ecd9080606015894d55f447847228
Kemal Eren <kemal@kemaleren.com>
2016-02-17 16:50:00 -0800

    banded matrix always includes top left and bottom right corners

--- quiver2.py
+++ quiver2.py
@@ -9,30 +9,29 @@ import numpy as np
 # TODO: benchmark against actual quiver implementation
 
 
-class OutsideBandError(Exception):
-    pass
-
-
-class OutOfBoundsError(Exception):
-    pass
-
-
 class BandedMatrix:
     """A sparse banded matrix.
+
+    Band is always wide enough to include [0, 0] and [-1, -1].
 
     Currently only supports limited slicing and no arithmetic
     operations.
 
+    Warning: does not do bounds checks.
+
     """
 
-    def __init__(self, shape, bandwidth, offset=0):
+    def __init__(self, shape, bandwidth=0):
         nrows, ncols = shape
         if not nrows > 0 and ncols > 0:
             raise Exception('shape is not positive: {}'.format(shape))
         self.shape = shape
         self.bandwidth = bandwidth
-        self.data = np.zeros((2 * bandwidth + 1, ncols))
-        self.offset = offset
+        self.diff = np.abs(nrows - ncols)
+        data_nrows = 2 * bandwidth + self.diff + 1
+        self.data = np.zeros((data_nrows, ncols))
+        self.h_offset = max(ncols - nrows, 0)
+        self.v_offset = max(nrows - ncols, 0)
 
     @property
     def nrows(self):
@@ -42,153 +41,54 @@ class BandedMatrix:
     def ncols(self):
         return self.shape[1]
 
-    def check_row(self, i):
-        if i < 0:
-            i = self.nrows + i
-        if i >= self.nrows:
-            raise OutOfBoundsError()
-        return i
-
-    def check_col(self, j, newcol=False):
-        """if newcol is true, allow j=ncols, for inserting a new column"""
-        if j < 0:
-            j = self.ncols + j
-        if not j < self.ncols + (1 if newcol else 0):
-            raise OutOfBoundsError()
-        return j
-
-    def square_row_range(self, j, newcol=False):
-        """array-indexed theoretical row range of band in column j, assuming
-        square matrix and band extending above first row and below
-        last.
-
-        """
-        j = self.check_col(j, newcol)
-        start = j - self.bandwidth - self.offset
-        stop = j - self.offset + self.bandwidth + 1
-        return start, stop
-
-    def row_range(self, j, newcol=False):
-        """array-indexed real row range of band in column j"""
-        # FIXME: assumes every column contains nonzero entries
-        start, stop = self.square_row_range(j, newcol)
-        start = max(0, start)
-        stop = min(stop, self.nrows)
-        assert start < stop
-        return start, stop
-
-    def inband(self, i, j):
-        start, stop = self.row_range(j)
-        return start <= i < stop
-
-    def _convert_row(self, i, j):
-        """convert array row to data row"""
-        if not self.inband(i, j):
-            raise OutsideBandError()
-        start, stop = self.row_range(j)
-        result = i - (j - self.bandwidth - self.offset)
-        assert 0 <= result < self.data.shape[0]
-        return result
-
-    def data_row_range(self, j):
-        """row range for column j in self.data"""
-        j = self.check_col(j)
-        start, stop = self.row_range(j)
-        dstart = self._convert_row(start, j)
-        dstop = self._convert_row(stop - 1, j) + 1
-        assert dstart < dstop
-        return dstart, dstop
+    def data_row(self, i, j):
+        return (i - j) + self.h_offset + self.bandwidth
 
     def get_elt(self, i, j):
-        i = self.check_row(i)
-        j = self.check_col(j)
-        if self.inband(i, j):
-            row = self._convert_row(i, j)
-            return self.data[row, j]
-        return 0
+        row = self.data_row(i, j)
+        return self.data[row, j]
+
+    def row_range(self, j):
+        start = max(0, j - self.h_offset - self.bandwidth)
+        stop = min(j + self.v_offset + self.bandwidth + 1, self.nrows)
+        return start, stop
+
+    def data_row_range(self, j):
+        start = max(0, self.h_offset + self.bandwidth - j)
+        a, b = self.row_range(j)
+        stop = start + (b - a)
+        return start, stop
 
     def get_col(self, j):
-        j = self.check_col(j)
-        start, stop = self.data_row_range(j)
-        return self.data[start:stop, j]
-
-    def __getitem__(self, key):
-        # TODO: only supports limited slicing
-        # TODO: return BandedMatrix
-        i, j = key
-        if not isinstance(j, int):
-            raise Exception()
-        if isinstance(i, int):
-            return self.get_elt(i, j)
-        elif i == slice(None, None, None):
-            return self.get_col(j)
-        else:
-            raise Exception('given slicing not recognized: {}'.format(key))
+        result = np.zeros(self.nrows)
+        start, stop = self.row_range(j)
+        dstart, dstop = self.data_row_range(j)
+        result[start:stop] = self.data[dstart:dstop, j]
+        return result
 
     def __setitem__(self, key, val):
         # TODO: implement multidim slicing and negative indices
         i, j = key
-        i = self.check_row(i)
-        j = self.check_col(j)
-        row = self._convert_row(i, j)
+        row = self.data_row(i, j)
         self.data[row, j] = val
 
-    def todense(self):
+    def inband(self, i, j):
+        row = self.data_row(i, j)
+        return 0 <= row < self.data.shape[0]
+
+    def full(self):
+        # this is slow, but just for testing purposes
         result = np.zeros((self.nrows, self.ncols))
-        for j in range(self.ncols):
-            start, stop = self.row_range(j)
-            dstart, dstop = self.data_row_range(j)
-            result[start:stop, j] = self.data[dstart:dstop, j]
+        for i in range(self.nrows):
+            for j in range(self.ncols):
+                if not self.inband(i, j):
+                    continue
+                row = self.data_row(i, j)
+                result[i, j] = self.data[row, j]
         return result
 
-    def col_indices(self, i):
-        """Generate all band indices"""
-        i = self.check_row(i)
-        # FIXME: this is inefficient
-        for j in range(self.ncols):
-            if self.inband(i, j):
-                yield j
-
-    def row_indices(self, j):
-        """Generate all band indices"""
-        j = self.check_col(j)
-        start, stop = self.row_range(j)
-        return range(start, stop)
-
-    def indices(self):
-        """Generate all band indices in [1:, 1:]"""
-        for j in range(1, self.ncols):
-            for i in range(*self.row_range(j)):
-                if i > 0:
-                    yield i, j
-
-    def predecessors(self, i, j):
-        """Generate all left, up, and left-up band indices"""
-        mods = ((-1, 0), (0, -1), (-1, -1))
-        for imod, jmod in mods:
-            ii = i + imod
-            jj = j + jmod
-            if ii < 0 or jj < 0:
-                continue
-            if self.inband(ii, jj):
-                yield ii, jj
-
     def flip(self):
-        # FIXME: not symmetric
         self.data = np.flipud(np.fliplr(self.data))
-        self.offset += divmod(self.nrows - self.ncols, 2)[1]
-
-
-class CenteredBandedMatrix(BandedMatrix):
-    """A banded matrix guaranteed to have the band touch elements [0, 0] and [-1, -1], plus some optional extra."""
-    def __init__(self, shape, bandwidth):
-        self.given_bandwidth = bandwidth
-        nrows, ncols = shape
-        diff = ncols - nrows
-        offset, r = divmod(diff, 2)
-        self.extra_bandwidth = max(bandwidth, r)
-        bandwidth = self.extra_bandwidth + np.abs(offset)
-        super().__init__(shape, bandwidth, offset=offset)
 
 
 def update(arr, i, j, s_base, t_base, log_p, log_ins, log_del):
@@ -197,35 +97,28 @@ def update(arr, i, j, s_base, t_base, log_p, log_ins, log_del):
     scores = []
     if arr.inband(i - 1, j):
         # insertion
-        scores.append(arr[i - 1, j] + log_ins)
+        scores.append(arr.get_elt(i - 1, j) + log_ins)
     if arr.inband(i, j - 1):
         # deletion
-        scores.append(arr[i, j - 1] + log_del)
+        scores.append(arr.get_elt(i, j - 1) + log_del)
     if arr.inband(i - 1, j - 1):
-        scores.append(arr[i - 1, j - 1] + sub)
+        scores.append(arr.get_elt(i - 1, j - 1) + sub)
     return max(scores)
 
 
 def forward(s, log_p, t, log_ins, log_del, bandwidth):
-    result = CenteredBandedMatrix((len(s) + 1, len(t) + 1), bandwidth)
-    for i in result.row_indices(0):
+    result = BandedMatrix((len(s) + 1, len(t) + 1), bandwidth)
+    for i in range(min(result.shape[0], result.v_offset + bandwidth + 1)):
         result[i, 0] = log_ins * i
-    for j in result.col_indices(0):
+    for j in range(min(result.shape[1], result.h_offset + bandwidth + 1)):
         result[0, j] = log_del * j
-    for i, j in result.indices():
-        result[i, j] = update(result, i, j, s[i-1], t[j-1], log_p[i-1], log_ins, log_del)
+    # TODO: reverse order of iteration?
+    for j in range(1, result.ncols):
+        start, stop = result.row_range(j)
+        start = max(start, 1)
+        for i in range(start, stop):
+            result[i, j] = update(result, i, j, s[i-1], t[j-1], log_p[i-1], log_ins, log_del)
     return result
-
-
-def updated_col(pos, base, template, seq, log_p,
-                A, B, log_ins, log_del, bandwidth):
-    Acols = BandedMatrix((A.nrows, 2), A.bandwidth, offset=A.offset - pos)
-    Acols.data[:, 0] = A.data[:, pos]
-    if Acols.inband(0, 0) and Acols.inband(0, 1):
-        Acols[0, 1] = Acols[0, 0] + log_del
-    for i in Acols.row_indices(1):
-        Acols[i, 1] = update(Acols, i, 1, seq[i-1], base, log_p[i-1], log_ins, log_del)
-    return Acols[:, 1]
 
 
 def backward(s, log_p, t, log_ins, log_del, bandwidth):
@@ -235,6 +128,50 @@ def backward(s, log_p, t, log_ins, log_del, bandwidth):
     result = forward(s, log_p, t, log_ins, log_del, bandwidth)
     result.flip()
     return result
+
+
+def updated_col(pos, base, template, seq, log_p, A, log_ins, log_del):
+    j = pos + 1
+    result = np.zeros(A.nrows)
+    if j < A.h_offset + A.bandwidth:
+        result[0] = log_del * j
+    start, stop = A.row_range(j)
+    # TODO: code duplication with `update()`
+    for i in range(start, stop):
+        sub = (0 if base == seq[i-1] else log_p[i-1])
+        scores = []
+        if A.inband(i - 1, j):
+            # insertion
+            scores.append(result[i - 1] + log_ins)
+        if A.inband(i, j - 1):
+            # deletion
+            scores.append(A.get_elt(i, j - 1) + log_del)
+        if A.inband(i - 1, j - 1):
+            scores.append(A.get_elt(i - 1, j - 1) + sub)
+        result[i] = max(scores)
+    return result
+
+
+def score_mutation(mutation, template, seq, log_p, A, B, log_ins, log_del, bandwidth):
+    """Score a mutation using the forward-backward trick."""
+    # FIXME: account for different size of array for an insertion or deletion
+    mtype, pos, base = mutation
+    if mtype == 'deletion':
+        aj = pos
+        Acol = A.get_col(aj)
+    else:
+        aj = pos + 1
+        Acol = updated_col(pos, base, template, seq, log_p, A, log_ins, log_del)
+    bj = pos + (0 if mtype == 'insertion' else 1)
+    Bcol = B.get_col(bj)
+
+    # need to chop off beginning of Acol and end of Bcol and align
+    a_start, a_stop = A.row_range(aj)
+    b_start, b_stop = B.row_range(bj)
+
+    start = max(a_start, b_start)
+    stop = min(a_stop, b_stop)
+    return (Acol[start:stop] + Bcol[start:stop]).max()
 
 
 def mutations(template):
@@ -250,31 +187,6 @@ def mutations(template):
     # insertion after last
     for base in 'ACGT':
         yield ['insertion', len(template), base]
-
-
-def score_mutation(mutation, template, seq, log_p, A, B, log_ins, log_del, bandwidth):
-    """Score a mutation using the forward-backward trick."""
-    mtype, pos, base = mutation
-    if mtype == 'deletion':
-        aj = pos
-        Acol = A[:, aj]
-    else:
-        aj = pos + 1
-        Acol = updated_col(pos, base, template, seq, log_p, A, B, log_ins, log_del, bandwidth)
-    bj = pos + (0 if mtype == 'insertion' else 1)
-    Bcol = B[:, bj]
-
-    # need to chop off beginning of Acol and end of Bcol and align
-    a_start, a_stop = A.row_range(aj, newcol=(mtype == "insertion" and aj == A.shape[1]))
-    b_start, b_stop = B.row_range(bj)
-
-    amin = max(b_start - a_start, 0)
-    amax = len(Acol) - max(a_stop - b_stop, 0)
-
-    bmin = max(a_start - b_start, 0)
-    bmax = len(Bcol) - max(b_stop - a_stop, 0)
-
-    return (Acol[amin:amax] + Bcol[bmin:bmax]).max()
 
 
 def update_template(template, mutation):
@@ -305,7 +217,7 @@ def apply_mutations(template, mutations):
     return template
 
 
-def choose_candidates(candidates, min_dist)
+def choose_candidates(candidates, min_dist):
     final_cands = []
     posns = set()
     for c in reversed(sorted(candidates, key=lambda c: c[0])):
--- test_quiver2.py
+++ test_quiver2.py
@@ -10,6 +10,7 @@ from quiver2 import update_template
 from quiver2 import forward
 from quiver2 import backward
 from quiver2 import score_mutation
+from quiver2 import updated_col
 
 
 def random_seq(length):
@@ -17,7 +18,9 @@ def random_seq(length):
 
 
 def score_slow(template, sequence, log_p, log_ins, log_del, bandwidth):
-    return forward(sequence, log_p, template, log_ins, log_del, bandwidth)[-1, -1]
+    A = forward(sequence, log_p, template, log_ins, log_del, bandwidth)
+    nrows, ncols = A.shape
+    return A.get_elt(nrows - 1, ncols - 1)
 
 
 class TestQuiver2(unittest.TestCase):
@@ -29,7 +32,7 @@ class TestQuiver2(unittest.TestCase):
         template = 'AA'
         seq = 'AA'
         log_p = np.repeat(-3, len(seq))
-        A = forward(seq, log_p, template, log_ins, log_del, bandwidth=1).todense()
+        A = forward(seq, log_p, template, log_ins, log_del, bandwidth=1).full()
         expected = np.array([[  0, -10, 0],
                              [ -5,   0, -10],
                              [0,  -5,   0]])
@@ -41,7 +44,7 @@ class TestQuiver2(unittest.TestCase):
         template = 'AA'
         seq = 'AT'
         log_p = np.repeat(-3, len(seq))
-        B = backward(seq, log_p, template, log_ins, log_del, bandwidth=1).todense()
+        B = backward(seq, log_p, template, log_ins, log_del, bandwidth=1).full()
         expected = np.array([[-3, -5, 0],
                              [-13, -3, -5],
                              [0, -10, 0]])
@@ -53,11 +56,33 @@ class TestQuiver2(unittest.TestCase):
         template = 'AA'
         seq = 'AT'
         log_p = np.repeat(-3, len(seq))
-        A = forward(seq, log_p, template, log_ins, log_del, bandwidth=1).todense()
+        A = forward(seq, log_p, template, log_ins, log_del, bandwidth=1).full()
         expected = np.array([[  0, -10, 0],
                              [ -5,  0,  -10],
                              [0, -5,  -3]])
         assert_array_equal(A, expected)
+
+    def test_updated_col_substitution(self):
+        log_del = -10
+        log_ins = -5
+        template = 'AAAA'
+        seq = 'ATAA'
+        log_p = np.repeat(-3, len(seq))
+        A = forward(seq, log_p, template, log_ins, log_del, bandwidth=1)
+        result = updated_col(1, 'T', template, seq, log_p, A, log_ins, log_del)
+        expected = forward(seq, log_p, seq, log_ins, log_del, bandwidth=1).get_col(2)
+        assert_array_equal(result, expected)
+
+    def test_updated_col_insertion(self):
+        log_del = -10
+        log_ins = -5
+        template = 'AAA'
+        seq = 'AAAT'
+        log_p = np.repeat(-3, len(seq))
+        A = forward(seq, log_p, template, log_ins, log_del, bandwidth=1)
+        result = updated_col(3, 'T', template, seq, log_p, A, log_ins, log_del)
+        expected = forward(seq, log_p, seq, log_ins, log_del, bandwidth=1).get_col(4)
+        assert_array_equal(result, expected)
 
     def test_random_mutations(self):
         point_rate = 0.1
@@ -83,8 +108,10 @@ class TestQuiver2(unittest.TestCase):
             B = backward(seq, log_p, template, log_ins, log_del, bandwidth)
             score = score_mutation(mutation, template, seq, log_p, A, B, log_ins, log_del, bandwidth)
             score2 = score_slow(new_template, seq, log_p, log_ins, log_del, bandwidth)
-            score3 = forward(seq, log_p, new_template, log_ins, log_del, bandwidth)[-1, -1]
-            score4 = backward(seq, log_p, new_template, log_ins, log_del, bandwidth)[0, 0]
+            M = forward(seq, log_p, new_template, log_ins, log_del, bandwidth)
+            nr, nc = M.shape
+            score3 = M.get_elt(nr - 1, nc - 1)
+            score4 = backward(seq, log_p, new_template, log_ins, log_del, bandwidth).get_elt(0, 0)
             self.assertAlmostEqual(score, score2)
             self.assertAlmostEqual(score, score3)
             self.assertAlmostEqual(score, score4)
