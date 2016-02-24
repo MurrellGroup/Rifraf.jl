7089cb63871926e9d34a06c6ab6b3e4f7369b813
Kemal Eren <kemal@kemaleren.com>
2016-02-23 23:34:57 -0800

    wrote BandedArray in Julia

---
+++ BandedArray.jl
@@ -0,0 +1,68 @@
+module BandedArrayModule
+
+export BandedArray, sparsecol, data_row, data_row_range, inband, full, flip!
+
+immutable BandedArray{T} <: AbstractArray{T,1}
+    data::Array{T}
+    shape::NTuple{2,Int}
+    bandwidth::Int
+    diff::Int
+    h_offset::Int
+    v_offset::Int
+
+    function BandedArray(shape, bandwidth)
+        nrows, ncols = shape
+        diff = abs(nrows - ncols)
+        data_nrows = 2 * bandwidth + diff + 1
+        dshape = (data_nrows, ncols)
+        h_offset = max(ncols - nrows, 0)
+        v_offset = max(nrows - ncols, 0)
+
+        return new(zeros(T, dshape), shape, bandwidth, diff, h_offset, v_offset)
+    end
+end
+
+Base.size(A::BandedArray) = A.shape
+
+Base.getindex{T}(A::BandedArray{T}, i::Int, j::Int) = get(A.data, (data_row(A, i, j), j), zero(T))
+
+Base.setindex!{T}(A::BandedArray{T}, v, i::Int, j::Int) = (A.data[data_row(A, i, j), j] = v)
+
+function sparsecol(A::BandedArray, j)
+    start, stop = data_row_range(A, j)
+    return A.data[start:stop, j]
+end
+
+data_row(A::BandedArray, i, j) = (i - j) + A.h_offset + A.bandwidth + 1
+
+function row_range(A::BandedArray, j)
+    start = max(1, j - A.h_offset - A.bandwidth)
+    stop = min(j + A.v_offset + A.bandwidth, size(A)[1])
+    return start, stop
+end
+
+function data_row_range(A::BandedArray, j)
+    start = max(1, A.h_offset + A.bandwidth - j)
+    a, b = row_range(A, j)
+    stop = start + (b - a)
+    return start, stop
+end
+
+inband(A::BandedArray, i, j) = 1 <= data_row(A, i, j) <= size(A.data)[1]
+
+function Base.full{T}(A::BandedArray{T})
+    result = zeros(T, size(A))
+    for j = 1:size(A)[2]
+        start, stop = row_range(A, j)
+        dstart, dstop = data_row_range(A, j)
+        result[start:stop, j] = A.data[dstart:dstop, j]
+    end
+    return result
+end
+
+function flip{T}(A::BandedArray{T})
+    newdata = flipdim(flipdim(A.data, 1), 2)
+    return BandedArray{T}(newdata, A.shape, A.bandwidth, A.diff, A.h_offset, A.v_offset)
+end
+
+end
---
+++ tests/BandedArray.jl
@@ -0,0 +1,90 @@
+include("../BandedArray.jl")
+
+using BandedArrayModule
+
+using Base.Test
+
+# test inband
+begin
+    m = BandedArray{Int32}((13, 11), 5)
+    @test inband(m, 1, 1)
+    @test inband(m, 8, 1)
+    @test ! inband(m, 9, 1)
+    @test inband(m, 1, 6)
+    @test ! inband(m, 1, 7)
+end
+
+#test_sym
+begin
+    m = BandedArray{Int32}((3, 3), 0)
+    m.data[:] = 1
+    @test full(m) == eye(3)
+end
+
+#test_sym_band
+begin
+    m = BandedArray{Int32}((3, 3), 1)
+    m.data[:] = 1
+    expected = ones(Int32, (3, 3))
+    expected[3, 1] = 0
+    expected[1, 3] = 0
+    @test full(m) == expected
+end
+
+# #test_wide
+begin
+    m = BandedArray{Int32}((3, 4), 0)
+    m.data[:] = 1
+    expected = zeros(Int32, (3, 4))
+    expected[1, 1:2] = 1
+    expected[2, 2:3] = 1
+    expected[3, 3:4] = 1
+    @test full(m) == expected
+end
+
+#test_wide_band
+begin
+    m = BandedArray{Int32}((3, 5), 0)
+    m.data[:] = 1
+    expected = zeros(Int32, (3, 5))
+    expected[1, 1:3] = 1
+    expected[2, 2:4] = 1
+    expected[3, 3:5] = 1
+    @test full(m) == expected
+end
+
+#test_wide_col
+begin
+    m = BandedArray{Int32}((3, 5), 1)
+    m.data[:] = 1
+    first = ones(Int32, 2)
+    middle = ones(Int32, 3)
+    last = first
+    @test sparsecol(m, 1) == first
+    @test sparsecol(m, 2) == middle
+    @test sparsecol(m, 3) == middle
+    @test sparsecol(m, 4) == middle
+    @test sparsecol(m, 5) == last
+end
+
+#test_tall
+begin
+    m = BandedArray{Int32}((4, 3), 0)
+    m.data[:] = 1
+    expected = zeros(Int32, (4, 3))
+    expected[1, 1:1] = 1
+    expected[2, 1:2] = 1
+    expected[3, 2:3] = 1
+    expected[4, 3:3] = 1
+    @test full(m) == expected
+end
+
+#test_tall_band
+begin
+    m = BandedArray{Int32}((5, 3), 1)
+    m.data[:] = 1
+    expected = ones(Int32, (5, 3))
+    expected[5, 1] = 0
+    expected[1, 3] = 0
+    @test full(m) == expected
+end
