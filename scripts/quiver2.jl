eca70dcf276aaf9d26252f25e0130849d73705ec
Kemal Eren <kemal@kemaleren.com>
2016-05-15 22:27:35 -0400

    add option to drop non-unique prefix and suffix

--- scripts/quiver2.jl
+++ scripts/quiver2.jl
@@ -12,9 +12,13 @@ function parse_commandline()
     s = ArgParseSettings()
     @add_arg_table s begin
         "--prefix"
-        help = "prependedto each filename to make label"
+        help = "prepended to each filename to make label"
         arg_type = AbstractString
         default = ""
+
+        "--keep-unique-name"
+        help = "keep only unique middle part of filename"
+        action = :store_true
 
         "--bandwidth"
         help = "alignment bandwidth"
@@ -74,12 +78,33 @@ end
     return consensus
 end
 
+function common_prefix(strings)
+    minlen = minimum([length(s) for s in strings])
+    cand = strings[1][1:minlen]
+    x = 0
+    for i = 1:minlen
+        if all([s[i] == cand[i] for s in strings])
+            x = i
+	else
+	    break
+        end
+    end
+    return cand[1:x]
+end
+
+function common_suffix(strings)
+    return reverse(common_prefix([reverse(s) for s in strings]))
+end
+
 function main()
     args = parse_commandline()
     input = args["input"]
 
     dir, pattern = splitdir(input)
     infiles = glob(pattern, dir)
+    if length(infiles) == 0
+       return
+    end
     names = [splitext(basename(f))[1] for f in infiles]
     if length(Set(names)) != length(names)
         error("Files do not have unique names")
@@ -90,9 +115,19 @@ function main()
     end
     results = pmap((f, a) -> dofile(f, a), infiles, [args for i in 1:length(infiles)])
 
+    plen = 0
+    slen = 0
+    if args["keep-unique-name"]
+        plen = length(common_prefix(names))
+	slen = length(common_suffix(names))
+    end
+
     prefix = args["prefix"]
     for i=1:length(results)
-        name = splitext(basename(infiles[i]))[1]
+        name = names[i]
+        if args["keep-unique-name"]
+            name = name[plen + 1:end - slen]
+        end
         label = string(prefix, name)
         write(STDOUT, Seq.FASTASeqRecord(label, results[i], Seq.FASTAMetadata("")))
     end
