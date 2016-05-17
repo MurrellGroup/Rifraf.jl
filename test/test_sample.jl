00b817074061e1ce6662b42c7117c802f9a95395
Kemal Eren <kemal@kemaleren.com>
2016-05-16 20:30:51 -0700

    test codon sampling

---
+++ test/test_sample.jl
@@ -0,0 +1,20 @@
+using Quiver2.Sample
+
+using Base.Test
+
+srand(1)
+
+# test that codon sampling is always a multiple of 3
+begin
+    for i in 1:100
+        template = random_seq(3 * 100)
+        error_rate = 0.1
+        sub_ratio = 1
+        ins_ratio = 3
+        del_ratio = 3
+        seq, logp = sample_from_template(template, error_rate,
+                                         sub_ratio, ins_ratio, del_ratio;
+                                         beta=-1.0, codon=true)
+        @test length(seq) % 3 == 0
+    end
+end
