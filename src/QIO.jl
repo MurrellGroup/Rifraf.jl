cdba43abd0e3547793b8125e48f70d0d064b2f94
Kemal Eren <kemal@kemaleren.com>
2017-01-19 11:53:14 -0800

    fastq names

--- src/QIO.jl
+++ src/QIO.jl
@@ -43,19 +43,25 @@ function read_fastq(filename)
     records = read_fastq_records(filename)
     seqs = DNASequence[]
     phreds = Vector{Int8}[]
+    names = String[]
     for record in records
         push!(seqs, record.seq)
         push!(phreds, record.metadata.quality)
+        push!(names, record.name)
     end
-    return seqs, phreds
+    return seqs, phreds, names
 end
 
-function write_fastq(filename, seqs, phreds::Vector{Vector{Int8}})
+function write_fastq(filename, seqs, phreds::Vector{Vector{Int8}};
+                     names=String[])
     stream = open(FASTQWriter, filename, quality_encoding=:sanger)
     i = 0
-    for (s, q) in zip(seqs, phreds)
+    if length(names) != length(seqs)
+        names = [string("seq_", i) for i in 1:length(seqs)]
+    end
+    for (s, q, n) in zip(seqs, phreds, names)
         i += 1
-        write(stream, Seq.FASTQSeqRecord(string("seq_", i), s, q))
+        write(stream, Seq.FASTQSeqRecord(n, s, q))
     end
     close(stream)
 end
