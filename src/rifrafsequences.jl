e1534ca40b2fd843e4dd14f780a84fc463ab99d6
Kemal Eren <kemal@kemaleren.com>
2017-02-20 19:03:32 -0500

    wip: precompute all scores

--- src/rifrafsequences.jl
+++ src/rifrafsequences.jl
@@ -4,12 +4,14 @@ error probabilities.
 """
 type RifrafSequence
     seq::DNASeq
-    error_log_p::Vector{LogProb}
     match_log_p::Vector{LogProb}
+    mismatch_log_p::Vector{LogProb}
+    ins_log_p::Vector{LogProb}
+    del_log_p::Vector{LogProb}
     bandwidth::Int
 
     function RifrafSequence(seq::DNASeq, error_log_p::Vector{LogProb},
-                            bandwidth::Int)
+                            bandwidth::Int, scores::Scores)
         if bandwidth < 1
             error("bandwidth must be positive")
         end
@@ -28,19 +30,23 @@ type RifrafSequence
             error("a log error probability is > 0: $bad_value")
         end
         match_log_p = log10(1.0 - exp10(error_log_p))
-        return new(seq, error_log_p, match_log_p, bandwidth)
+        mismatch_log_p = error_log_p + scores.mismatch
+        ins_log_p = error_log_p + scores.insertion
+        del_log_p = Vector{LogProb}(length(error_log_p) + 1)
+        del_log_p[1] = error_log_p[1]
+        for i=1:length(error_log_p) - 1)
+            del_log_p[i + 1] = max(error_log_p[i], error_log_p[i+1])
+        end
+        del_log_[end] = error_log_p[end]
+        return new(seq, match_log_p, mismatch_log_p, ins_log_p, del_log_p, bandwidth)
     end
 end
 
-function RifrafSequence(seq::DNASeq, phreds::Vector{Int8}, bandwidth::Int)
+function RifrafSequence(seq::DNASeq, phreds::Vector{Int8}, bandwidth::Int, scores::Scores)
     error_log_p = phred_to_log_p(phreds)
-    return RifrafSequence(seq, error_log_p, bandwidth)
+    return RifrafSequence(seq, error_log_p, bandwidth, scores)
 end
 
 function length(s::RifrafSequence)
     return length(s.seq)
-end
-
-function reverse(s::RifrafSequence)
-    return RifrafSequence(reverse(s.seq), reverse(s.error_log_p), s.bandwidth)
 end
